f5ae3707ccacede6b13533c6e77b83cb03562aad
Edgar Duran <edgarduran@Edgars-MacBook-Pro.local>
2015-09-18 16:56:02 -0600

    Fixed merge conflicts

---
+++ lib/crack.rb
@@ -0,0 +1,41 @@
+class Crack
+
+  def tail_position(encrypted)
+    remainder = encrypted.length % 4
+    if remainder == 0
+      rotation_a
+    elsif remainder == 1
+      rotation_b
+    elsif remainder == 2
+      rotation_c
+    else
+      rotation_d
+    end 
+
+  end
+
+  def
+    character_map = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y','z',
+                    '0', '1', '2', '3', '4', '5', '6', '7','8','9', ' ', '.', ',']
+  end
+
+  def set_rotation_a
+
+  end
+
+  def set_rotation_b
+
+  end
+
+  def set_rotation_c
+
+  end
+
+  def set_rotation_d
+
+  end
+
+  def
+
+end
--- lib/decryptor.rb
+++ lib/decryptor.rb
@@ -6,11 +6,7 @@ class Decrypt
                     '0', '1', '2', '3', '4', '5', '6', '7','8','9', ' ', '.', ',']
     array = []
     counter = 0
-<<<<<<< HEAD
-    message.each do |letter|
-=======
     message.map do |letter|
->>>>>>> c273545f305f3f7cf5bb0dfe6a6606b0f7a207a3
       if !character_map.include?(letter)
         array << letter
       else
@@ -22,11 +18,3 @@ class Decrypt
     array
   end
 end
-<<<<<<< HEAD
-
-
-text = Decrypt.new
-result = text.decryptor(["3", "2", "m", ",", "h", "3", "w", "r", "k", "s", "x", "y", "2", "b", "c"], [10, 60, 4, 98])
-result == ["t", "h", "i", "s", " ", "i", "s", " ", "a", " ", "t", "e", "s", "t", "."]
-=======
->>>>>>> c273545f305f3f7cf5bb0dfe6a6606b0f7a207a3
