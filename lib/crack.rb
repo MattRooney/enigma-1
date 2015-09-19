a8e8b32e3dee317876c84ded0e3d31fd63f2fd9b
Edgar Duran <edgarduran@Edgars-MacBook-Pro.local>
2015-09-18 19:32:33 -0600

    completed crack

--- lib/crack.rb
+++ lib/crack.rb
@@ -1,41 +1,78 @@
+require_relative 'decryptor'  # => true
+
 class Crack
 
-  def tail_position(encrypted)
-    remainder = encrypted.length % 4
-    if remainder == 0
-      rotation_a
-    elsif remainder == 1
-      rotation_b
-    elsif remainder == 2
-      rotation_c
-    else
-      rotation_d
-    end 
+  attr_accessor :crack_a, :crack_b, :crack_c, :crack_d, :character_map, :message  # => nil
 
-  end
+  def initialize(message)
+    @message = message     # => "iza.pewqpewq"
+  end                      # => :initialize
 
-  def
-    character_map = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y','z',
-                    '0', '1', '2', '3', '4', '5', '6', '7','8','9', ' ', '.', ',']
-  end
+  def character_map
+    @character_map = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',   # => "m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m", "m"
+                        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y','z',  # => "z", "z", "z", "z", "z", "z", "z", "z", "z", "z", "z", "z"
+                        '0', '1', '2', '3', '4', '5', '6', '7','8','9', ' ', '.', ',']   # => ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", ".", ","], ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", ".", ","], ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", ".", ","], ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", ".", ","], ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",...
+  end                                                                                    # => :character_map
 
-  def set_rotation_a
+  def tail_position
+    remainder = message.length % 4  # => 0
+  end                               # => :tail_position
 
-  end
+  def tail_rotation
+    message[-1].chars.map do |letter|                                                  # => ["q"]
+    pos_char_map = character_map.index(letter)                                         # => 16
+    @tail_rotation = (pos_char_map - character_map.index('.')) % character_map.length  # => 18
+    end                                                                                # => [18]
+  end                                                                                  # => :tail_rotation
 
-  def set_rotation_b
+  def neg2_rotation
+    message[-2].chars.map do |letter|                                                  # => ["w"]
+    pos_char_map = character_map.index(letter)                                         # => 22
+    @neg2_rotation = (pos_char_map - character_map.index('.')) % character_map.length  # => 24
+    end                                                                                # => [24]
+  end                                                                                  # => :neg2_rotation
 
-  end
+  def neg3_rotation
+    message[-3].chars.map do |letter|                                                  # => ["e"]
+    pos_char_map = character_map.index(letter)                                         # => 4
+    @neg3_rotation = (pos_char_map - character_map.index('d')) % character_map.length  # => 1
+    end                                                                                # => [1]
+  end                                                                                  # => :neg3_rotation
 
-  def set_rotation_c
+  def neg4_rotation
+    message[-4].chars.map do |letter|                                                  # => ["p"]
+    pos_char_map = character_map.index(letter)                                         # => 15
+    @neg4_rotation = (pos_char_map - character_map.index('n')) % character_map.length  # => 2
+    end                                                                                # => [2]
+  end                                                                                  # => :neg4_rotation
 
-  end
+  # def unordered_array
+  #   unordered_array = []
+  #   unordered_array << @neg4_rotation
+  #   unordered_array << @neg3_rotation
+  #   unordered_array << @neg2_rotation
+  #   unordered_array << @tail_rotation
+  # end
 
-  def set_rotation_d
+  def crack
+    crack = []                                                                       # => []
+    if tail_position == 0                                                            # => true
+      crack << @neg4_rotation << @neg3_rotation << @neg2_rotation << @tail_rotation  # => [2, 1, 24, 18]
+    elsif tail_position == 1
+      crack << @tail_rotation << @neg4_rotation << @neg3_rotation << @neg2_rotation
+    elsif tail_position == 2
+      crack << @neg2_rotation << @tail_rotation << @neg4_rotation << @neg3_rotation
+    elsif tail_position == 3
+      crack << @neg3_rotation << @neg2_rotation << @tail_rotation << @neg4_rotation
+    end                                                                              # => [2, 1, 24, 18]
+  end                                                                                # => :crack
 
-  end
 
-  def
+end  # => :crack
 
-end
+array = Crack.new("iza.pewqpewq")  # => #<Crack:0x007f7fe2806ab0 @message="iza.pewqpewq">
+array.tail_rotation                # => [18]
+array.neg2_rotation                # => [24]
+array.neg3_rotation                # => [1]
+array.neg4_rotation                # => [2]
+array.crack                        # => [2, 1, 24, 18]
--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -1,170 +1,121 @@
-require 'date'  # => true
+require 'date'
 
 class Key
 
-  attr_accessor :key  # => nil
+  attr_accessor :key
 
   def initialize
-    @key = key    # => nil
-  end             # => :initialize
+    @key = key
+  end
 
   def random_generator
-    array = []           # => []
-    5.times do           # => 5
-      array << rand(10)  # => [3], [3, 4], [3, 4, 5], [3, 4, 5, 4], [3, 4, 5, 4, 6]
-    end                  # => 5
-    @key = array         # => [3, 4, 5, 4, 6]
-  end                    # => :random_generator
+    array = []
+    5.times do
+      array << rand(10)
+    end
+    @key = array
+  end
 
-end  # => :random_generator
+end
 
 class Offsets
 
-  attr_accessor :offsets, :integer_date  # => nil
+  attr_accessor :offsets, :integer_date
 
   def initialize
-    @offsets = offsets            # => nil
-    @integer_date = integer_date  # => nil
-  end                             # => :initialize
+    @offsets = offsets
+    @integer_date = integer_date
+  end
 
   def offsets_array
-    date = Date.today.strftime("%d%m%y")                 # => "170915"
-    integer_date = date.to_i                             # => 170915
-    squared_date = integer_date ** 2                     # => 29211937225
-    squared_string = squared_date.to_s                   # => "29211937225"
-    squared_array = squared_string.split("").map do |n|  # => ["2", "9", "2", "1", "1", "9", "3", "7", "2", "2", "5"]
-      n.to_i                                             # => 2, 9, 2, 1, 1, 9, 3, 7, 2, 2, 5
-    end                                                  # => [2, 9, 2, 1, 1, 9, 3, 7, 2, 2, 5]
-    squared_array.last(4)                                # => [7, 2, 2, 5]
-  end                                                    # => :offsets_array
-end                                                      # => :offsets_array
-
-class Encryptor
-<<<<<<< HEAD
-  Character_Map = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y','z',
-                  '0', '1', '2', '3', '4', '5', '6', '7','8','9', ' ', '.', ',']
-=======
-  attr_accessor :key, :offsets, :key_a, :key_b, :key_c, :key_d  # => nil
->>>>>>> c273545f305f3f7cf5bb0dfe6a6606b0f7a207a3
-
-  def initialize
-    @key     = Key.new.random_generator   # => [3, 4, 5, 4, 6]
-    @offsets = Offsets.new.offsets_array  # => [7, 2, 2, 5]
-  end                                     # => :initialize
-
-  def key_a
-    @key = @key.map { |i| i.to_s}     # => ["3", "4", "5", "4", "6"], ["3", "4", "5", "4", "6"], ["3", "4", "5", "4", "6"]
-    key_a_string = @key[0] + @key[1]  # => "34", "34", "34"
-    @key_a = key_a_string.to_i        # => 34, 34, 34
-  end                                 # => :key_a
-
-  def key_b
-    @key = @key.map { |i| i.to_s}     # => ["3", "4", "5", "4", "6"], ["3", "4", "5", "4", "6"], ["3", "4", "5", "4", "6"]
-    key_a_string = @key[1] + @key[2]  # => "45", "45", "45"
-    @key_b = key_a_string.to_i        # => 45, 45, 45
-  end                                 # => :key_b
-
-  def key_c
-    @key = @key.map { |i| i.to_s}     # => ["3", "4", "5", "4", "6"], ["3", "4", "5", "4", "6"], ["3", "4", "5", "4", "6"]
-    key_a_string = @key[2] + @key[3]  # => "54", "54", "54"
-    @key_c = key_a_string.to_i        # => 54, 54, 54
-  end                                 # => :key_c
-
-  def key_d
-    @key = @key.map { |i| i.to_s}     # => ["3", "4", "5", "4", "6"], ["3", "4", "5", "4", "6"], ["3", "4", "5", "4", "6"]
-    key_a_string = @key[3] + @key[4]  # => "46", "46", "46"
-    @key_d = key_a_string.to_i        # => 46, 46, 46
-  end                                 # => :key_d
-
-  def key_array
-    @key_array = []              # => []
-    @key_array.insert(0, key_a)  # => [34]
-    @key_array.insert(1, key_b)  # => [34, 45]
-    @key_array.insert(2, key_c)  # => [34, 45, 54]
-    @key_array.insert(3, key_d)  # => [34, 45, 54, 46]
-    @key_array                   # => [34, 45, 54, 46]
-  end                            # => :key_array
-
-  def offsets_a
-    @offsets_a = @offsets[0]  # => 7, 7
-  end                         # => :offsets_a
-
-  def offsets_b
-    @offsets_b = @offsets[1]  # => 2, 2
-  end                         # => :offsets_b
-
-  def offsets_c
-    @offsets_c = @offsets[2]  # => 2, 2
-  end                         # => :offsets_c
-
-  def offsets_d
-<<<<<<< HEAD
-    @offsets_d = @offsets[3]
-  end
-
-  def encryption_a
-    @encryption_a = key_a + offsets_a
-  end
-
-  def encryption_b
-    @encryption_b = key_b + offsets_b
-  end
-
-  def encryption_c
-    @encryption_c = key_c + offsets_c
-  end
-
-  def encryption_d
-    @encryption_d = key_d + offsets_d
-  end
-
-  def original_message
-    # take string of original message and seperate it by character
-  end
-
-  def encrypt_message
-    # reset character values according to encryption number(s)
+    date = Date.today.strftime("%d%m%y")
+    integer_date = date.to_i
+    squared_date = integer_date ** 2
+    squared_string = squared_date.to_s
+    squared_array = squared_string.split("").map do |n|
+      n.to_i
+    end
+    squared_array.last(4)
   end
 end
 
-offsets = Offsets.new
+class Encryptor
+
+  attr_accessor :key, :offsets, :key_a, :key_b, :key_c, :key_d
+
+
+  def initialize
+    @key     = Key.new.random_generator
+    @offsets = Offsets.new.offsets_array
+  end
+
+  def key_a
+    @key = @key.map { |i| i.to_s}
+    key_a_string = @key[0] + @key[1]
+    @key_a = key_a_string.to_i
+  end
+
+  def key_b
+    @key = @key.map { |i| i.to_s}
+    key_a_string = @key[1] + @key[2]
+    @key_b = key_a_string.to_i
+  end
+
+  def key_c
+    @key = @key.map { |i| i.to_s}
+    key_a_string = @key[2] + @key[3]
+    @key_c = key_a_string.to_i
+  end
+
+  def key_d
+    @key = @key.map { |i| i.to_s}
+    key_a_string = @key[3] + @key[4]
+    @key_d = key_a_string.to_i
+  end
+
+  def key_array
+    @key_array = []
+    @key_array.insert(0, key_a)
+    @key_array.insert(1, key_b)
+    @key_array.insert(2, key_c)
+    @key_array.insert(3, key_d)
+    @key_array
+  end
+
+  def offsets_a
+    @offsets_a = @offsets[0]
+  end
+
+  def offsets_b
+    @offsets_b = @offsets[1]
+  end
+
+  def offsets_c
+    @offsets_c = @offsets[2]
+  end
+
+  def offsets_d
+    @offsets_d = @offsets[3]
+end
+
+  def rotation
+    @rotation = []
+    @rotation.insert(0, key_a + offsets_a)
+    @rotation.insert(1, key_b + offsets_b)
+    @rotation.insert(2, key_c + offsets_c)
+    @rotation.insert(3, key_d + offsets_d)
+    @rotation
+  end
+end
+
 value = Encryptor.new
 value.key_a
 value.key_b
 value.key_c
 value.key_d
+value.key_array
 value.offsets_a
 value.offsets_b
 value.offsets_c
 value.offsets_d
-value.encryption_a
-value.encryption_b
-value.encryption_c
-value.encryption_d
-=======
-    @offsets_d = @offsets[3]  # => 5, 5
-  end                         # => :offsets_d
-
-  def rotation
-    @rotation = []                          # => []
-    @rotation.insert(0, key_a + offsets_a)  # => [41]
-    @rotation.insert(1, key_b + offsets_b)  # => [41, 47]
-    @rotation.insert(2, key_c + offsets_c)  # => [41, 47, 56]
-    @rotation.insert(3, key_d + offsets_d)  # => [41, 47, 56, 51]
-    @rotation                               # => [41, 47, 56, 51]
-  end                                       # => :rotation
-end                                         # => :rotation
-
-value = Encryptor.new  # => #<Encryptor:0x007fa0a48f1e88 @key=[3, 4, 5, 4, 6], @offsets=[7, 2, 2, 5]>
-value.key_a            # => 34
-value.key_b            # => 45
-value.key_c            # => 54
-value.key_d            # => 46
-value.key_array        # => [34, 45, 54, 46]
-value.offsets_a        # => 7
-value.offsets_b        # => 2
-value.offsets_c        # => 2
-value.offsets_d        # => 5
-value.rotation         # => [41, 47, 56, 51]
->>>>>>> c273545f305f3f7cf5bb0dfe6a6606b0f7a207a3
+value.rotation
